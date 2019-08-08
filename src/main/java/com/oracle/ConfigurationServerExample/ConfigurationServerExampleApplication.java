cea9c1c0d1c09a9cfcb1254df42403e1b7dd2f7a
Anmol Bajaj <anmol.bajaj@oracle.com>
2019-08-08 09:51:15 +0530

    addding prokject fiels

---
+++ src/main/java/com/oracle/ConfigurationServerExample/ConfigurationServerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.ConfigurationServerExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationServerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServerExampleApplication.class, args);
+	}
+
+}
